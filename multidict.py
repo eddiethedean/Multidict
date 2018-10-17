1539bd6481bdaa22c75c10e1a396b5e1d6da433e
Odos Matthews <odosmatthews.gmail.com>
2018-10-17 18:20:59 -0400

    added dict inher

--- multidict.py
+++ multidict.py
@@ -1,36 +1,24 @@
-class Multidict:
+class Multidict(dict):
     """Class that behaves like a dictionary that can hold multiple values for each key"""
     def __init__(self, iterable=(), **kwargs):
         self.md = {}
         self.update(iterable, **kwargs)
         
-    def __repr__(self):
-        return str(self.md)
-        
-    def __getitem__(self, key): #to get a value by its key
-        return self.md[key]
-        
-    def __setitem__(self, key, value): #to set a value by its key
+    def __setitem__(self, key, value):
         if key in self:
-            self.md[key].append(value)
+            self[key].append(value)
         else:
-            self.md[key] = [value]
-            
-    def __delitem__(self, key): #to delete a key-value pair
+            dict.__setitem__(self, key, [value])
+
+    def __delitem__(self, key):
         if key in self:
             if len(self[key])>1:
                 del self[key][0]
             else:
-                self.md.pop(key)
-    
+                dict.__delitem__(self, key)
+        
     def delete_key(self, key):
         self.md.pop(key)
-                
-    def __missing__(self, nonexistent_key): #to provide a default value for missing keys
-        return -1
-    
-    def __contains__(self, key):
-        return key in self.md
     
     def __len__(self):
         """len() returns number of different values"""
@@ -43,7 +31,6 @@ class Multidict:
                 iterable = iterable.items()
             for key,val in iterable:
                 self[key] = val
-                
         if kwargs:
             for key,val in kwargs.items():
                 self[key]=val
@@ -54,9 +41,12 @@ class Multidict:
         
     def pop(self, key):
         if key in self:
-            temp = key, self[key][0]
-            del self[key]
-            return temp
+            if len(self[key])>1:
+                temp = [self[key][0]]
+                del self[key][0]
+                return temp
+            else:
+                return dict.pop(self, key)
         
     def pop_key(self, key):
         if key in self:
@@ -65,16 +55,10 @@ class Multidict:
             return temp
 
     def items(self):
-        return [(key, item) for key, val in self.md.items() for item in val]
-
-    def keys(self):
-        return self.md.keys()
+        return [(key, item) for key, val in dict.items(self) for item in val]
     
     def values(self):
-        return [item for val in self.md.values() for item in val]
-    
-    def clear(self):
-        self.md = {}
+        return [item for val in dict.values(self) for item in val]
     
     def get_first(self, key):
         """returns first value for key"""
@@ -83,28 +67,10 @@ class Multidict:
     def get_last(self, key):
         """returns last value for key"""
         return self[key][-1]
-    
-    def count(self, key):
-        """returns number of values for key"""
-        return len(self[key])
         
 if __name__=='__main__':
     new_d = Multidict([('cat','Nova'),('cat','Zora'),('dog','Nami')])
-    new_d = Multidict(cat='Nova',dog='Nami')
-    new_d['cat']='Zora'
-    new_d.update([('human','Odos')])
-    new_d.update(human='Odos')
-    new_d.update(human='Kayla')
-    print(new_d.items())
-    print(new_d.values())
     print(new_d)
-    print('pop', new_d.pop('cat'))
-    print(new_d)
-    print('pop', new_d.pop('cat'))
-    print(new_d)
-    print(len(new_d))
-
-    sec_d = Multidict(ape='bob', lizard='godzilla')
-    new_d.append(sec_d)
-    print(new_d)
-    print(new_d.get_first('human'))
+    new_c = Multidict(cat='Nova',dog='Nami')
+    print(new_c)
+    
